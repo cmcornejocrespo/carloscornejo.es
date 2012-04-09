78e4470e76248f9746b7f31ce587ec6a7d3c062a
Carlos M. Cornejo <withnodelay@yahoo.com>
2012-04-09 13:46:33 +0100

    changes made to add * twitter feature. * http loading of js resources. * add js obfuscated.

---
+++ js/js2.js
@@ -0,0 +1,67 @@
+/**
+ * @author Carlos M.
+ */
+window.addEvent('domready', function() {
+
+	$$('.toggleSection').each(function(el){
+	    var slidetrigger = el.getElements('.toggler');
+	    var slidebody = el.getElement('.toggleInfo');
+	    if(slidebody!=null && slidetrigger.length>0){
+	        var slidewrapper = new Element('div').wraps(slidebody);
+	        var linkClass = (el.hasClass('expanded') ? 'expanded' : 'collapsed');
+	        var slideTween = new Fx.Tween(slidewrapper, {
+	            duration: 'long',
+	            transition: 'bounce:out',
+	            link: 'cancel',
+	            onComplete: function(){if(linkClass=='expanded'){slidewrapper.erase('style');}}
+	        });
+	        el.addClass(linkClass);
+	        slidetrigger.setStyle('cursor', 'pointer');
+	        slidetrigger.addEvent('click', function(e){
+	            slidewrapper.get('tween').removeEvents('complete');
+	            slidewrapper.setStyle('overflow', 'hidden');
+	            el.removeClass(linkClass);
+	            if(linkClass=='expanded'){
+	                linkClass = 'collapsed';
+	                slideTween.start('height', 0);
+	            } else {
+	                linkClass = 'expanded';
+	                slideTween.start('height', slidebody.getSize().y);
+	            }
+	            el.addClass(linkClass);
+	        });
+	        if(linkClass=='collapsed'){slidewrapper.setStyles({'height': '0', 'overflow':'hidden'});}
+	    }
+	});
+	
+	$$('.toggleSectionInner').each(function(el){
+	    var slidetrigger = el.getElements('.togglerInner');
+	    var slidebody = el.getElement('.toggleInfoInner');
+	    if(slidebody!=null && slidetrigger.length>0){
+	        var slidewrapper = new Element('div').wraps(slidebody);
+	        var linkClass = (el.hasClass('expanded') ? 'expanded' : 'collapsed');
+	        var slideTween = new Fx.Tween(slidewrapper, {
+	            duration: 'long',
+	            transition: 'bounce:out',
+	            link: 'cancel',
+	            onComplete: function(){if(linkClass=='expanded'){slidewrapper.erase('style');}}
+	        });
+	        el.addClass(linkClass);
+	        slidetrigger.setStyle('cursor', 'pointer');
+	        slidetrigger.addEvent('click', function(e){
+	            slidewrapper.get('tween').removeEvents('complete');
+	            slidewrapper.setStyle('overflow', 'hidden');
+	            el.removeClass(linkClass);
+	            if(linkClass=='expanded'){
+	                linkClass = 'collapsed';
+	                slideTween.start('height', 0);
+	            } else {
+	                linkClass = 'expanded';
+	                slideTween.start('height', slidebody.getSize().y);
+	            }
+	            el.addClass(linkClass);
+	        });
+	        if(linkClass=='collapsed'){slidewrapper.setStyles({'height': '0', 'overflow':'hidden'});}
+	    }
+	});
+});
